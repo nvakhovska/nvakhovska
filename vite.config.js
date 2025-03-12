840f6bcc15066e10a05a79f83dd0ce065fd568bb
nvakhovska <nataliia.vakhovska@gmail.com>
2025-03-12 18:09:16 -0400

    Added contact me

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nvakhovska/",
-  define: {
-    VITE_BASE_URL: JSON.stringify("/nvakhovska"),
-  },
+  base: "/",
 });
